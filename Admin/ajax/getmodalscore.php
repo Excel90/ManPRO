6b71c23561156d6779ee98a7e381ddcc73163d5d
LymoniusPantheum <c14190053@john.petra.ac.id>
2021-12-03 00:58:16 +0700

    getmodalscore yang benar

--- Admin/ajax/getmodalscore.php
+++ Admin/ajax/getmodalscore.php
@@ -48,7 +48,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                                 </div>
                             <div class="col-sm-6 col-12">
                                 <div class="mb-3">
-                                    <input type="number" class="form-control score" value="'.$scores['percentage'].'">
+                                    <input type="number" class="form-control score" value="'.$scores['score'].'">
                                 </div>
                             </div>';
                         }
